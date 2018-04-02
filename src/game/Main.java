dfdb24fd3f3deeffc9f7ded94e190f90d43ad154
Althoumb <s-ballw@bsd405.org>
2018-04-01 18:01:40 -0700

    framerate is now limited to the highest refresh rate of all monitors

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,9 @@
 package game;
 
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
@@ -7,9 +11,18 @@ public class Main {
 	public static final String GAME_NAME = "BeET";
 
 	public static void main(String[] args) {
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gs = ge.getScreenDevices();
+		int refreshRate = 0;
+		for (GraphicsDevice element : gs) {
+			DisplayMode dm = element.getDisplayMode();
+			if (dm.getRefreshRate() > refreshRate) {
+				refreshRate = dm.getRefreshRate();
+			}
+		}
 		try {
 			AppGameContainer app = new AppGameContainer(new Game());
-			app.setTargetFrameRate(144);
+			app.setTargetFrameRate(refreshRate);
 			app.setIcons(new String[] { "res/icons/iconsmall.png",
 					"res/icons/iconlarge.png" });
 			app.start();
